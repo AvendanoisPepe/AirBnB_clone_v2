6c0a05c8ad2e6c50296e6320165a1718d4163cf1
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-21 22:58:34 -0500

    pepe

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,10 +4,11 @@ que distribuya un archivo a sus servidores web, usando la función do_deploy:
 """
 from fabric.api import local, run, put, env
 from datetime import datetime
-import os
+from os import path
 
 env.hosts = ["34.73.84.99", "3.93.174.191"]
 env.user = "ubuntu"
+
 
 def do_pack():
     """Creamos la carpeta versions usando fabric y sus comandos
@@ -23,25 +24,18 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """
-    Function that distributes an archive.
-    """
-    name_dir = archive_path[9:-4]
-    if os.path.exists(archive_path):
+    
+    if path.isfile(archive_path):
+        name_file = archive_path[9:]
+        new_path = "/data/web_static/releases/"
+        path_server_file = '/tmp/{}'.format(name_file)
         put(archive_path, '/tmp/')
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(name_dir))
-        run("sudo tar -xzf /tmp/{}.tgz -C \
-        /data/web_static/releases/{}/".format(name_dir, name_dir))
-        run("sudo rm /tmp/{}.tgz".format(name_dir))
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".
-            format(name_dir, name_dir))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(name_dir))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current".format(name_dir))
+        run('sudo mkdir -p {}'.format(new_path))
+        run("sudo tar -xzf {} -C {}/".format(path_server_file, new_path))
+        run('sudo rm {}'.format(path_server_file))
+        run('sudo rm -rf {}'.format('/data/web_static/current'))
+        run('sudo ln -s {} /data/web_static/current'.format(new_path))
         print("New version deployed!")
         return True
-    else:
-        return False
+
+    return False
