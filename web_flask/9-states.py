9a881a495688c8a8cfbca9ea92e01c4044db045d
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-24 12:50:04 -0500

    segun yo ya

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,23 +6,26 @@ from models import storage
 from models.state import State
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def cerrar(self):
     """Elimina la sesión actual de SQLAlchemy"""
     storage.close()
 
+
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def lista(id=None):
-    """Con la funcion all muestra una lista de todos los estados"""
+    """Con la funcion all muestra una lista de todos los estados
+    y verificando si manda el id o no mostramos ya sea un registro
+    o todos o ninguno."""
     states = storage.all(State).values()
-    if id:
-        llave = "State." + id
-        if llave in states:
-            estado = states[llave]
-        else:
-            estado = None
-    return(render_template("9-states.html", states=states))
+    if id is not None:
+        for estado in states:
+            if estado.id == id:
+                return(render_template("9-states.html", states=estado))
+        return(render_template("9-states.html"))
+    return(render_template("9-states.html", states=states, vari=True))
 
 
 if __name__ == "__main__":
